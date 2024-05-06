ece4c04c56b97dbe44c054dcfdd82678d31bc422
Siqi Li <2605059174@qq.com>
2024-05-06 13:12:00 +0800

    Add F std and brute

---
+++ day1/F/lsq/brute.cpp
@@ -0,0 +1,96 @@
+#include<cstdio>
+#include<cstring>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+const int maxn=100005,maxm=200005;
+struct Edge{
+    int v,w,nt;
+    Edge(int v=0,int w=0,int nt=0):
+        v(v),w(w),nt(nt){}
+}e[maxm*2];
+int hd[maxn],num=1;
+void qwq(int u,int v,int w){
+    e[++num]=Edge(v,w,hd[u]),hd[u]=num;
+}
+int q[maxn],dis[maxn],n;
+int bfs(int s,int t){
+    for(int i=1;i<=n;++i)
+        dis[i]=0;
+    int fro=0,bac=0;
+    dis[q[bac++]=s]=1;
+    while(fro<bac){
+        int u=q[fro++];
+        for(int i=hd[u];i;i=e[i].nt){
+            int v=e[i].v;
+            if(e[i].w&&!dis[v]){
+                dis[q[bac++]=v]=dis[u]+1;
+            }
+        }
+    }
+    return dis[t];
+}
+int cur[maxn];
+int dfs(int u,int sm,int t){
+    if(u==t)return sm;
+    int re=0;
+    for(int&i=cur[u];i;i=e[i].nt){
+        int v=e[i].v,w=e[i].w;
+        if(dis[v]==dis[u]+1&&w){
+            int fw=dfs(v,min(sm,w),t);
+            sm-=fw;e[i].w-=fw;
+            re+=fw;e[i^1].w+=fw;
+        }
+    }
+    return re;
+}
+int dinic(int s,int t){
+    int re=0;
+    while(bfs(s,t)){
+        for(int i=1;i<=n;++i)cur[i]=hd[i];
+        re+=dfs(s,0x3f3f3f3f,t);
+    }
+    return re;
+}
+void sear(int u,int t){
+    printf("%d%c",u," \n"[u==t]);
+    if(u==t)return;
+    for(int i=hd[u];i;i=e[i].nt){
+        int v=e[i].v,w=e[i].w;
+        if(!w){
+            ++e[i].w;--e[i^1].w;
+            return sear(v,t);
+        }
+    }
+}
+int main(){
+    // freopen("test.in","r",stdin);
+    int T;scanf("%d",&T);
+    while(T--){
+        int m;
+        scanf("%d%d",&n,&m);
+        for(int i=1;i<=m;++i){
+            int u,v;
+            scanf("%d%d",&u,&v);
+            qwq(u,v,1);qwq(v,u,1);
+        }
+        int k=(m+n-2)/(n-1),ok=false;
+        for(int s=1;s<=n&&!ok;++s){
+            for(int t=s+1;t<=n&&!ok;++t){
+                if(dinic(s,t)>=k){
+                    printf("%d %d\n",s,t);
+                    for(int i=1;i<=k;++i)
+                        sear(s,t);
+                    ok=true;
+                }
+                for(int i=2;i<=num;++i)
+                    e[i].w=1;
+            }
+        }
+        if(!ok)puts("-1");
+        num=1;
+        for(int i=1;i<=n;++i)
+            hd[i]=0;
+    }
+    return 0;
+}
---
+++ day1/F/lsq/std.cpp
@@ -0,0 +1,83 @@
+#include<cstdio>
+#include<cstring>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+const int maxm=200005;
+int f[maxm*3],n,st[maxm*3];
+int get(int t,int x){
+    x=x+t*n;
+    int tp=0;
+    while(f[x]!=x){
+        st[++tp]=x;
+        x=f[x];
+    }
+    while(tp)
+        f[st[tp--]]=x;
+    return x;
+}
+struct Edge{
+    int v,nt;
+    Edge(int v=0,int nt=0):
+        v(v),nt(nt){}
+}e[maxm*6];
+int hd[maxm*3],num;
+void qwq(int u,int v){
+    e[++num]=Edge(v,hd[u]);
+    hd[u]=num;
+}
+void link(int t,int x,int y){
+    f[get(t,x)]=get(t,y);
+    x=x+t*n,y=y+t*n;
+    qwq(x,y);qwq(y,x);
+}
+int dfs(int u,int p,int tar){
+    int re=(u==tar);
+    for(int i=hd[u];i;i=e[i].nt){
+        int v=e[i].v;
+        if(v==p)continue;
+        re|=dfs(v,u,tar);
+    }
+    if(re)printf("%d%c",(u-1)%n+1," \n"[p==0]);
+    return re;
+}
+int main(){
+    // freopen("test.in","r",stdin);
+    int T;scanf("%d",&T);
+    while(T--){
+        int m;
+        scanf("%d%d",&n,&m);
+        int k=(m+n-2)/(n-1),nk=n*k;
+        for(int i=1;i<=nk;++i)f[i]=i;
+        for(int i=1;i<=m;++i){
+            int u,v;
+            scanf("%d%d",&u,&v);
+            if(get(k-1,u)==get(k-1,v))continue;
+            int l=0,r=k-1;
+            while(l<r){
+                int mid=(l+r)/2;
+                if(get(mid,u)==get(mid,v))l=mid+1;
+                else r=mid;
+            }
+            link(l,u,v);
+        }
+        int s=0,t=0;
+        for(int i=1;i<=n;++i){
+            if(get(k-1,i)!=(k-1)*n+i){
+                s=i,t=(get(k-1,i)-1)%n+1;
+            }
+        }
+        if(s==0){
+            puts("-1");
+        }
+        else{
+            printf("%d %d\n",s,t);
+            for(int i=0;i<k;++i){
+                dfs(i*n+t,0,i*n+s);
+            }
+        }
+        for(int i=1;i<=nk;++i)hd[i]=0;
+        num=0;
+    }
+    return 0;
+}

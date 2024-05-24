be4abd6a6ab015c7edb39d8ffe43db04f2712a67
Eliminate Space <925529119@qq.com>
2024-05-24 18:01:56 +0800

    C data

---
+++ day1/C/esc/gen6.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#define cmin(a,b) (a>(b)?a=(b),1:0)
+#define cmax(a,b) (a<(b)?a=(b),1:0)
+#define dmin(a,b) ((a)<(b)?(a):(b))
+#define dmax(a,b) ((a)>(b)?(a):(b))
+namespace io{
+	int F(){
+		int n=0,F=0;
+		char ch;
+		while((ch=getchar())!='-'&&(ch<'0'||ch>'9'));
+		ch=='-'?F=1:n=ch-'0';
+		while((ch=getchar())>='0'&&ch<='9')n=n*10+ch-'0';
+		return F?-n:n;
+	}
+	long long G(){
+		long long n=0,F=0;
+		char ch;
+		while((ch=getchar())!='-'&&(ch<'0'||ch>'9'));
+		ch=='-'?F=1:n=ch-'0';
+		while((ch=getchar())>='0'&&ch<='9')n=n*10+ch-'0';
+		return F?-n:n;
+	}
+}
+std::random_device device;
+std::mt19937_64 gen(device());
+long long R(long long l, long long r){
+	return std::uniform_int_distribution<long long>(l,r)(gen);
+}
+int main(){
+	int n=200000;
+    printf("%d\n",n);
+    for(int i=1;i<=n;++i){
+        printf("%d%c",R(1,100000000)," \n"[i==n]);
+    }
+    for(int i=2;i<=n;++i){
+        printf("%d%c",i<=100000?1:100000," \n"[i==n]);
+    }
+	return 0;
+}

c385389f54ab9e13fc9e802c3b01b5aae9e1d021
ckw20 <78785236+ckw20@users.noreply.github.com>
2024-05-24 18:03:38 +0800

    Update std.cpp

--- day1/C/ckw/std.cpp
+++ day1/C/ckw/std.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 inline int inn() { int x;scanf("%d",&x);return x; }
 const int N=200010;vector<int> g[N];long long ans[N],w[N];int sz[N];
-inline bool cmp(int x,int y) { return ans[x]>ans[y]; }
+inline bool cmp(int x,int y) { return w[x]>w[y]; }
 void dfs(int x)
 {
 	for(auto y:g[x]) dfs(y);
